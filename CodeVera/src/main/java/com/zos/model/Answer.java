da9ad747b6edd89d2654911a089df4e48277fcfe
ThilinaWi <thilinawicki962@gmail.com>
2025-05-01 08:29:26 +0530

    change some answerpart

---
+++ CodeVera/src/main/java/com/zos/model/Answer.java
@@ -0,0 +1,31 @@
+package com.zos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String author;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
