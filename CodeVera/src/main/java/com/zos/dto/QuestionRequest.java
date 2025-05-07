ed70a90ed6737aa25a53cea713fe89fd68aec490
ThilinaWi <thilinawicki962@gmail.com>
2025-05-07 08:54:19 +0530

    add some new

---
+++ CodeVera/src/main/java/com/zos/dto/QuestionRequest.java
@@ -0,0 +1,10 @@
+package com.zos.dto;
+
+import java.util.List;
+
+public class QuestionRequest {
+    public String title;
+    public String body;
+    public Long userId;
+    public List<String> tags;
+}
